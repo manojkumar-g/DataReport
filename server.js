c41df441d748650fab4cc110021369f9ef7ccfbe
Manoj Kumar <manoj.g.kumar@hotmail.com>
2017-04-02 15:59:09 +0530

    final change to app

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ var app = express();
 connection(conf.dbUri);
 var compiler = webpack(config);
 
+
 app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  noInfo : true,
+  hot : true
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
